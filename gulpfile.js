cb2b24993314ef648148e4e1af6141f77a0443c5
Willian Teophilo <wteophilo@gmail.com>
2016-08-10 22:37:51 -0300

    add glup

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true,
+    port: 8081
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('./app/**/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function () {
+  gulp.src('./app/**/*.css')
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+  gulp.src('./app/**/*.js')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./app/**/*.html'], ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
